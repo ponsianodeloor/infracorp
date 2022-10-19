b465ecd7c7238dc9776861ed4d61e5037a24ac89
Ponsiano De Loor <ponsianodeloor@gmail.com>
2022-10-19 18:00:37 -0500

    creacion de la plantilla AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/system', function () {
+        return view('system.index');
+    })->name('system');
 });
